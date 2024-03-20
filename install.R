d7c51790e69187ad91367d71def457b83173fa2a
Matt Begun <matthewbegun@gmail.com>
2024-03-20 11:08:51 +1000

    include R packages used through week 4

--- install.R
+++ install.R
@@ -1,6 +1,9 @@
 # Install required R packages for the analysis
 # At minimum include tidyverse so Binder knows we want an R kernel.
-install.packages("tidyverse")
-install.packages("pander")
 install.packages("carData")
+install.packages("DiagrammeR")
 install.packages("ggmap")
+install.packages("pander")
+install.packages("tidyverse")
+install.packages("vegan")
+install.packages("VennDiagram")
