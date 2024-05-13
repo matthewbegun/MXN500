0532de3c80fdd18f6b25825efdc2cd8ffb3c23f4
Matt Begun <matthewbegun@gmail.com>
2024-05-13 22:34:39 +1000

    ensure pacman

--- install.R
+++ install.R
@@ -1,9 +1,5 @@
 # Install required R packages for the analysis
 # At minimum include tidyverse so Binder knows we want an R kernel.
-install.packages("carData")
-install.packages("DiagrammeR")
-install.packages("ggmap")
-install.packages("pander")
 install.packages("tidyverse")
-install.packages("vegan")
-install.packages("VennDiagram")
+install.packages("languageserver")
+install.packages("pacman")
